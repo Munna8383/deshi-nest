9e2fd19f731a36ceead185db36fa18e94b1b3437
Munna8383 <jahedmunna612@gmail.com>
2024-08-15 17:45:28 +0600

    added footer and navbar

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,16 @@
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
 
 
 const LandingPage = () => {
     return (
         <div>
+            <Navbar></Navbar>
+
+
+
+
+            <Footer></Footer>
             
         </div>
     );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,43 @@
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="navbar bg-base-300 px-5">
+  <div className="flex-1">
+     <div>
+     <div className="flex justify-center items-center">
+                        <div>
+                            <img className="w-12 h-8" src="../../../public/—Pngtree—letter d logo_6928616.png" alt=""  />
+                        </div>
+                        <div>
+                            <h1 className="text-2xl font-extrabold">Deshi Nest</h1>
+                        </div>
+                    </div>
+     </div>
+  </div>
+  <div className="flex-none">
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
         </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
     );
 };
 
